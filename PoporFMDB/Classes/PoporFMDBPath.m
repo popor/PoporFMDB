6fa2b9fb3058d1f0425a1f21fd39bd23ac5becad
popor <908891024@qq.com>
2021-02-04 16:11:36 +0800

    1.12 更新iOS和macOS获取cache路径方法.

--- PoporFMDB/Classes/PoporFMDBPath.m
+++ PoporFMDB/Classes/PoporFMDBPath.m
@@ -21,30 +21,26 @@
 
 - (void)initPath {
     self.DBFileName = DBFileNameDefault;
-    
-    NSArray * pathsToDocuments = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    
-    self.projectPath = [pathsToDocuments objectAtIndex:0];
+    NSArray * pathsToDocuments = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    self.projectPath = pathsToDocuments.firstObject;
     // UI系列
 #if TARGET_OS_IOS || TARGET_OS_TV || TARGET_OS_WATCH
-    self.cachesPath  = [self.projectPath stringByReplacingOccurrencesOfString:@"/Documents" withString:@"/Library/Caches"];
-    
+    self.cachesPath = self.projectPath;
     // NS系列
 #elif TARGET_OS_MAC
-    NSRange range = [self.projectPath rangeOfString:@"/Library/Containers"];
-    if (range.location == NSIntegerMax) {
-        NSLog(@"PoporFMDB ⚠️ you did not open App sandbox, will use a default db path.");
-        self.projectPath = [NSString stringWithFormat:@"%@/%@/Data/Library/Caches", self.projectPath, [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"]];
-    }
-    
-    self.cachesPath  = self.projectPath;
-    
+    self.cachesPath = [NSString stringWithFormat:@"%@/%@", self.projectPath, [self getBundleID]];
 #endif
     
     self.DBPath = [NSString stringWithFormat:@"%@/%@", self.cachesPath, self.DBFileName];
     
-    NSLog(@"PoporFMDB cachesPath: %@", self.cachesPath);
-    NSLog(@"PoporFMDB mainDBPath: %@", self.DBPath);
+    NSLog(@"PoporFMDB cachesPath:\n%@", self.cachesPath);
+    NSLog(@"PoporFMDB mainDBPath:\n%@", self.DBPath);
 }
+
+//获取BundleID
+- (NSString*)getBundleID {
+    return [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
+}
+
 
 @end
